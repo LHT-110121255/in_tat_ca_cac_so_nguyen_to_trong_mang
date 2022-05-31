da2f78dc147c5dec4ca5f4890621c6b4da99f1e8
Lâm Huệ Trung font-end <100610960+LHT-110121255@users.noreply.github.com>
2022-05-31 13:45:19 +0700

    Add files via upload

---
+++ CTDL_SNT.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+bool mark[10001];
+bool snt(int n);
+void nhap(int *arr, int n);
+void xuat(int *arr, int n);
+void out_all_snt(int *arr, int n);
+
+int main()
+{
+	int n; 
+	printf("nhap kich thuoc  mang = ");
+	scanf("%d", &n);
+	int arr[n];
+	nhap(arr, n);
+	out_all_snt(arr, n);
+}
+void nhap(int *arr, int n)
+{
+	int i;
+	for(i = 1; i <= n; i++)
+	{
+		printf("nhap arr[%d]", i);
+		scanf("%d", &arr[i]);
+	}
+	printf("\nmangg vua nhap \n");
+	xuat(arr, n);
+}
+void xuat(int *arr, int n)
+{
+	int i;
+	for(i = 1; i <= n; i++)
+		printf("%d\t", arr[i]);
+}
+bool snt(int n)
+{
+	if( n <= 1) return false;
+	int a = 2; 
+	while(true)
+	{
+		if(a*a > n) break;
+		int p = a*a;
+		while(p <= n)
+		{
+			mark[p] = true;
+			p += a;
+		}
+		++a;
+		while(a <= n && mark[a]) ++a;
+		if(a > n) break;
+	}
+	return (mark[n]) ? false : true;
+}
+void out_all_snt(int *arr, int n)
+{
+	int i;
+	for(i = 1; i <= n; i++)
+		(snt(arr[i])) ? printf("\n%d la so nguyen to ", arr[i]) : printf("\n%d khong la so nguyen to", arr[i]);
+}
